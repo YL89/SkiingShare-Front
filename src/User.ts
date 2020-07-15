b941cd0216e0fc026782a6c117345874ca3e6cdb
Yuhao Lu <ylu159@myseneca.ca>
2020-07-14 22:32:56 -0400

    added data type User and Coach

---
+++ src/User.ts
@@ -0,0 +1,16 @@
+export  class User{ 
+    id: number;
+    firstName: String;
+    lastName: String;
+    email: String;
+    age: number;
+    postalCode: String;
+    address: String;
+    phone: String;
+    userName: String;
+    password: String;
+    balance: number;
+    coach: [];
+    item: [];
+    rentItem: [];
+}
