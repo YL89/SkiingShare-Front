75905486ffdf1dcf5b9340efe5699c36ad93f070
Yilidan Zeng <yilidan37@gmail.com>
2020-08-21 09:32:08 -0400

    Add ski, boot and pole class
    Signed-off-by: Yilidan Zeng <yilidan37@gmail.com>

---
+++ src/Ski.ts
@@ -0,0 +1,12 @@
+export class Ski{
+    id: number;
+    name: String;
+    images: Array<String>;
+    brand: String;
+    category: String; // All mountain, Carving, Park.
+    SkierLevel: String; // Beginner, Intermediate, Advanced, Expert.
+    Length: number;
+    Width:number;
+    Index:number;
+    AjustableSkiBootsSize:number;
+}
---
+++ src/SkiPole.ts
@@ -0,0 +1,3 @@
+export class SkiPole{
+    PoleLength: number;
+}
